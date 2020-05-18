90bd6d2ec61fcd4f44d9b4b9aadc025386b7b2b4
Dumon <dumon.dmd@gmail.com>
2020-05-18 11:58:10 -0300

    Utilizando belongsToMany - Os projetos dos desenvolvedores

--- app/Desenvolvedor.php
+++ app/Desenvolvedor.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Desenvolvedor extends Model
 {
+	//Quando o padrão de nomes das migrations é diferente do model
     protected $table = 'desenvolvedores';
+
+    function projetos() {
+    	return $this->belongsToMany("App\Projeto", "alocacoes");    	
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Projeto;
+use App\Alocacao;
+use App\Desenvolvedor;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/desenvolvedor_projeto', function () {
+	$desenvolvedores = Desenvolvedor::with('projetos')->get();	
+	
+	foreach ($desenvolvedores as $d) {
+		echo "<p>Nome do Desenvolvedor: " .$d->nome. "</p>";
+		echo "<p>Cargo: " .$d->cargo. "</p>";
+		if(count($d->projetos) > 0){
+			echo "Projetos: <br>";
+			echo "<ul>";
+			foreach ($d->projetos as $p) {
+				echo "<li>";
+				echo "Nome: " .$p->nome. " | ";
+				echo "Horas: " .$p->estimativa_horas;
+				echo "</li>";
+			}
+			echo "</ul>";
+		}
+
+		echo "<hr>";
+	}
+
 });
